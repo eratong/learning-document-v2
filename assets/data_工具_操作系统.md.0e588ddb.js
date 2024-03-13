3d5facce3fb8ca0187298f6c0a78d244fa221f7d
FM00894 <481696701@qq.com>
2024-03-13 18:50:09 +0800

    初始化二

---
+++ assets/data_工具_操作系统.md.0e588ddb.js
@@ -0,0 +1,19 @@
+import{_ as a,o as e,c as o,X as t}from"./chunks/framework.b5656a4e.js";const m=JSON.parse('{"title":"操作系统","description":"","frontmatter":{},"headers":[],"relativePath":"data/工具/操作系统.md","filePath":"data/工具/操作系统.md","lastUpdated":1697011110000}'),r={name:"data/工具/操作系统.md"},n=t(`<h1 id="操作系统" tabindex="-1">操作系统 <a class="header-anchor" href="#操作系统" aria-label="Permalink to &quot;操作系统&quot;">​</a></h1><h2 id="mac" tabindex="-1">mac <a class="header-anchor" href="#mac" aria-label="Permalink to &quot;mac&quot;">​</a></h2><h3 id="快捷键" tabindex="-1">快捷键 <a class="header-anchor" href="#快捷键" aria-label="Permalink to &quot;快捷键&quot;">​</a></h3><blockquote><p>navicat 侧边栏 ddl 查看</p><blockquote><p>command+2</p></blockquote><p>强制刷新</p><blockquote><p>command+shift+R</p></blockquote><p>截屏 mac自带的截屏 可以固定在屏幕上</p><blockquote><p>command+shift+5</p></blockquote></blockquote><h3 id="常用命令" tabindex="-1">常用命令 <a class="header-anchor" href="#常用命令" aria-label="Permalink to &quot;常用命令&quot;">​</a></h3><p>端口占用</p><p>停止进程</p><p>压缩解压rar 安装 <a href="https://www.rarlab.com/download.htm" target="_blank" rel="noreferrer">https://www.rarlab.com/download.htm</a> 选择 RAR 7.00 beta 2 for macOS x64 # 安装rar命令 sudo install -c -o $USER rar /usr/local/bin/ # 安装unrar命令 sudo install -c -o $USER unrar /usr/local/bin/ # 解压到当前目录 unrar x tong.rar</p><h2 id="window" tabindex="-1">window <a class="header-anchor" href="#window" aria-label="Permalink to &quot;window&quot;">​</a></h2><h3 id="快捷键-1" tabindex="-1">快捷键 <a class="header-anchor" href="#快捷键-1" aria-label="Permalink to &quot;快捷键&quot;">​</a></h3><h3 id="常用命令-1" tabindex="-1">常用命令 <a class="header-anchor" href="#常用命令-1" aria-label="Permalink to &quot;常用命令&quot;">​</a></h3><h2 id="linux" tabindex="-1">linux <a class="header-anchor" href="#linux" aria-label="Permalink to &quot;linux&quot;">​</a></h2><h3 id="常用命令-2" tabindex="-1">常用命令 <a class="header-anchor" href="#常用命令-2" aria-label="Permalink to &quot;常用命令&quot;">​</a></h3><h2 id="初始化常用软件" tabindex="-1">初始化常用软件 <a class="header-anchor" href="#初始化常用软件" aria-label="Permalink to &quot;初始化常用软件&quot;">​</a></h2><p>工具</p><blockquote><pre><code>1、cmder
+2、C盘清理 SpaceSniffer
+3、postman、apifox
+4、sublime
+5、editplus
+6、notepad
+7、截图录屏 fastStone capture portable
+8、xmind
+</code></pre></blockquote><p>linux相关</p><blockquote><pre><code>1、xhsell、xftp
+2、secureCRT 、secureFX
+3、FinalShell
+</code></pre></blockquote><p>数据库相关</p><blockquote><pre><code>1、redis desktop manager
+2、navicat
+</code></pre></blockquote><p>java相关</p><blockquote><pre><code>1、maven
+2、jdk
+3、intellj
+</code></pre></blockquote><p>测试相关</p><blockquote><pre><code>1、postman
+2、Charles 抓包
+</code></pre></blockquote>`,24),l=[n];function c(d,i,h,s,p,u){return e(),o("div",null,l)}const q=a(r,[["render",c]]);export{m as __pageData,q as default};
